4a9e58c1cb4bd9af8098584f27893cbff335c89a
KeutlwileCG <keutlwileangel96@gmail.com>
2023-06-21 08:30:05 -0700

    lopps

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
-
+#include main.h
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
 void print_alphabet_x10(void)
 {
-int i, j;
+char c, i;
 
-for (i = 0; i < 10; i++) 
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j < 26; j++) 
+for (c = 'a'; c <= 'z'; c++)
 {
-_putchar('a' + j);
+_putchar(c);
 }
 _putchar('\n');
 }
 }
+
