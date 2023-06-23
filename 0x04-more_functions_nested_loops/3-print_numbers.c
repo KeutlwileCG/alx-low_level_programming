373f9c4b8d983c2d6642fede2d9ba7b17cff015a
KeutlwileCG <135268276+KeutlwileCG@users.noreply.github.com>
2023-06-24 00:04:16 +0200

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -15,19 +15,3 @@ char c;
 	}
 	_putchar('\n');
 }
-
-
-3-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-print_numbers();
-return (0);
-}
