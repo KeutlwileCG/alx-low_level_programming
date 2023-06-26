6a8dc9df5a576c60371d1ccc79a3e9d2aebc0ad0
KeutlwileCG <135268276+KeutlwileCG@users.noreply.github.com>
2023-06-26 22:43:02 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,16 @@
-#includemain.h/**
-*_strlen-returnsthelengthofastring*
-@s:string*Return:length*/int_strlen(char*s)
-{intlongi=0;
-while(*s!='\0'){longi++;
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s)
+{
+int longi = 0;
+while (*s != '\0')
+{
+longi++;
 s++;
-}return(longi);
+}
+return (longi);
 }
