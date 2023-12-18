3cc571aece4395190fbd8feea818822a10fc0371
KeutlwileCG <keutlwileangel96@gmail.com>
2023-12-18 15:03:54 -0800

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
-int main(void)
-{
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
-}
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,4 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
