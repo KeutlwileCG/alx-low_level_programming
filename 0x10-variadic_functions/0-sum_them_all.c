997ff51b69bb13a24837236793aec5440ba7c3a2
KeutlwileCG <keutlwileangel96@gmail.com>
2023-12-23 05:22:50 -0800

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
-#include "variadic_functions.h"
-
+#include
+"variadic_functions.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* sum_them_all - adds all the numbers
+* @n: the number of parameters passed
+* Return: Always 0
+*/
+int sum_them_all(const unsigned int n,
+...)
 {
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);    
-    return (0);
+int sum;
+unsigned int i;
+va_list ap;
+va_start(ap, n);
+if (n == 0)
+return (0);
+for (i = 0; i < n; i++)
+sum += va_arg(ap, int);
+va_end(ap);
+return (sum);
 }
